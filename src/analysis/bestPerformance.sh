99a7e5d88cae9b4955a26259adbc1fb13a6d0bee
assaff <assaf.faragy@mail.huji.ac.il>
2011-01-24 10:41:01 +0200

    best performance script

--- src/analysis/bestPerformance.sh
+++ src/analysis/bestPerformance.sh
@@ -4,16 +4,18 @@ resultsDir=$1
 for k in `seq 1 8`; do
     outfile="$resultsDir/roc.best$k.clusters.txt"
     cat /dev/null > $outfile
-    echo -e "#PDB\tTPR\tSPECIFICITY" >> $outfile
+    echo -e "#PDB\tTPR\tFPR" >> $outfile
     for f in $resultsDir/*quality.txt; do
 	    filename=`basename $f`
 	    pdbid=${filename%%.*}
 	
 	    tpr=`cat $f | egrep -v '^#' | head -$k | sort -nrk 8 | head -1 | cut -f8`
         fpr=`cat $f | egrep -v '^#' | head -$k | sort -nrk 8 | head -1 | cut -f9`
+        
 	    #recall=`calc $tp/$tpfn`
 	    #specificity=`calc $tp/$tpfp`
 	    #echo $coverage $size $tp $tpfp $tpfn
+		echo -e "$pdbid\t$tpr\t$fpr"
 	    echo -e "$pdbid\t$tpr\t$fpr" >> $outfile
     done # | awk '{sum=sum+$1;}END{print sum/NR}'
 done
--- src/analysis/totalPerformance.sh
+++ src/analysis/totalPerformance.sh
@@ -4,17 +4,23 @@ resultsDir=$1
 for k in `seq 1 8`; do
     outfile="$resultsDir/roc.top$k.clusters.txt"
     cat /dev/null > $outfile
-    echo -e "#PDB\tTPR\tSPECIFICITY" >> $outfile
+    echo -e "#PDB\tTPR\tFPR" >> $outfile
     for f in $resultsDir/*quality.txt; do
 	    filename=`basename $f`
 	    pdbid=${filename%%.*}
 	
-	    tpr=`cat $f | egrep -v '^#' | head -$k | awk '{sum=sum+$8}END{print sum}'`
-        fpr=`cat $f | egrep -v '^#' | head -$k | awk '{sum=sum+$9}END{print sum}'`
+	    tp=`cat $f | egrep -v '^#' | head -$k | awk '{sum=sum+$4}END{print sum}'`
+        fp=`cat $f | egrep -v '^#' | head -$k | awk '{sum=sum+$5}END{print sum}'`
+        tn=`cat $f | egrep -v '^#' | head -$k | awk '{sum=sum+$6}END{print sum}'`
+        fn=`cat $f | egrep -v '^#' | head -$k | awk '{sum=sum+$7}END{print sum}'`
+        tpr=`calc $tp/\($tp+$fn\)`
+        fpr=`calc $fp/\($fp+$tn\)`
 	    #recall=`calc $tp/$tpfn`
 	    #specificity=`calc $tp/$tpfp`
 	    #echo $coverage $size $tp $tpfp $tpfn
+	    #echo -e "$pdbid\t$tpr\t$fpr"
 	    echo -e "$pdbid\t$tpr\t$fpr" >> $outfile
     done # | awk '{sum=sum+$1;}END{print sum/NR}'
 done
+exit
 

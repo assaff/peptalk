bb0255c717a7d13f9b0152b03366f98d28cde2fe
assaff <assaf.faragy@mail.huji.ac.il>
2011-02-17 14:42:59 +0200

    nothing much

---
+++ src/model_data/model.py
@@ -0,0 +1,12 @@
+__author__ = 'assaff'
+
+
+class Model:
+    '''
+        the starting point for structural analysis.
+        includes the structure itself, and any auxiliary data that might be appended to it
+    '''
+
+    def __init__(self, pdb_filename=None, receptor_chains=None, peptide_chains=None,):
+        pass
+

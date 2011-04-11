bf31c8468abdf714bd5701fde5a8443d5c05c172
assaff <assaf.faragy@mail.huji.ac.il>
2011-04-11 08:13:31 +0300

    regular commit

--- analysis/visual/visualize_data.sh
+++ analysis/visual/visualize_data.sh
@@ -2,13 +2,25 @@
 
 LOCAL_PDB_DIR=/vol/ek/share/pdb
 
+function getRemarks {
+
+local pdbid=$1
+getPdbLocal $pdbid | grep -e '^REMARK'
+}
+
 function visualize_data_pair {
 
     local bound_pdb=$(echo $1 | tr '[A-Z]' '[a-z]')
     local bc=$2
     local unbound_pdb=$(echo $3 | tr '[A-Z]' '[a-z]')
     local ubc=$4
+    
+    local bound_pdb_obj=$bound_pdb"_bound"
+    local unbound_pdb_obj=$unbound_pdb"_unbound"
 
+    local bound_chain_obj="bound_$bound_pdb"_"$bc"
+    local unbound_chain_obj="unbound_$unbound_pdb"_"$ubc"
+    local peptide_obj="peptide_"$bound_pdb
 
     local pmlfile="$bound_pdb.$unbound_pdb.pml"
     #echo $pmlfile
@@ -16,41 +28,45 @@ function visualize_data_pair {
     touch $pmlfile
 
     midid_b=${bound_pdb:1:2}
-    echo load $LOCAL_PDB_DIR/$midid_b/pdb$bound_pdb.ent.gz, $bound_pdb >> $pmlfile
-    echo select bound, $bound_pdb and chain $bc >> $pmlfile
+    echo load $LOCAL_PDB_DIR/$midid_b/pdb$bound_pdb.ent.gz, $bound_pdb_obj >> $pmlfile
+    echo select $bound_chain_obj, polymer and $bound_pdb_obj and chain $bc >> $pmlfile
 
     midid_ub=${unbound_pdb:1:2}
-    echo load $LOCAL_PDB_DIR/$midid_ub/pdb$unbound_pdb.ent.gz, $unbound_pdb >> $pmlfile
-    echo select unbound, $unbound_pdb and chain $ubc >> $pmlfile
+    echo load $LOCAL_PDB_DIR/$midid_ub/pdb$unbound_pdb.ent.gz, $unbound_pdb_obj >> $pmlfile
+    echo select $unbound_chain_obj, polymer and $unbound_pdb_obj and chain $ubc >> $pmlfile
 
-    echo align polymer and name ca and bound, polymer and name ca and unbound, quiet=0, object=\"aln_bound_unbound\", reset=1 >> $pmlfile
+    echo align polymer and name ca and $bound_chain_obj, polymer and name ca and $unbound_chain_obj, quiet=0, object=\"aln_bound_unbound\", reset=1 >> $pmlfile
 
-    echo select peptide, \($bound_pdb within 8 of unbound\) and not bound >> $pmlfile
+    echo select $peptide_obj, polymer and \($bound_pdb_obj within 8 of $unbound_chain_obj\) and not $bound_chain_obj >> $pmlfile
     echo deselect >> $pmlfile
-    echo color red, peptide >> $pmlfile
-    echo color green, bound >> $pmlfile
-    echo color blue, unbound >> $pmlfile
+    echo color lime, $bound_pdb_obj >> $pmlfile
+    echo color forest, $bound_chain_obj >> $pmlfile
+    echo color cyan, $unbound_pdb_obj >> $pmlfile
+    echo color blue, $unbound_chain_obj >> $pmlfile
+    echo color red, $peptide_obj >> $pmlfile
 
     echo show_as cartoon, all >> $pmlfile
 #    echo hide everything, not bound and not unbound and not peptide >> $pmlfile
-    echo "orient unbound;" >> $pmlfile
-    echo "create unb_surface, unbound; color white, unb_surface; set transparency, 0.6, unb_surface; show_as surface, unb_surface; hide everything, unb_surface" >> $pmlfile
-    echo "symexp sym, $unbound_pdb, ($unbound_pdb), 5.0" >> $pmlfile
+    echo "orient $unbound_pdb_obj;" >> $pmlfile
+    echo "create $unbound_pdb.surface, $unbound_pdb_obj; color white, $unbound_pdb.surface; set transparency, 0.6, $unbound_pdb.surface; show_as surface, $unbound_pdb.surface; hide everything, $unbound_pdb.surface" >> $pmlfile
+    echo "symexp sym, $unbound_pdb_obj, ($unbound_pdb_obj), 5.0" >> $pmlfile
     echo "show_as cartoon, sym*; color white, sym*" >> $pmlfile
     #echo "hide everything, sym*" >> $pmlfile
-    echo "select crystal_contacts, br. sym* within 5.0 of unbound; color pink, crystal_contacts; deselect;" >> $pmlfile
-        echo "select suspects, unbound within 5.0 of crystal_contacts; deselect; color hotpink, suspects" >> $pmlfile
-    echo "center peptide" >> $pmlfile
-    
-    getPdbLocal $unbound_pdb | grep 'REMARK 350' > $bound_pdb.$unbound_pdb.info
+    echo "select crystal_contacts, sym* within 4.0 of $unbound_chain_obj; color pink, crystal_contacts; deselect;" >> $pmlfile
+    echo "select suspects, $unbound_chain_obj within 4.0 of crystal_contacts; deselect; color hotpink, suspects" >> $pmlfile
+    echo "center $peptide_obj" >> $pmlfile
+
+    getRemarks $bound_pdb > remarks.$bound_pdb.info
+    getRemarks $unbound_pdb > remarks.$unbound_pdb.info
     #pymol -q $pmlfile &
 }
 
+function visualize_table {
 IFS=$'\n' 
 for line in $(cat ../mapping.clean.txt ); do 
     IFS=$' '
     visualize_data_pair $line
     #break
 done
-    
+    }
 

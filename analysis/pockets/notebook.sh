753eb7fada76bc93eb6d2cca4c7c0bf70536bcc5
Assaf Faragy <assaf.faragy@gmail.com>
2011-04-25 16:37:02 +0300

    pockets comparison - fpocket, castp

---
+++ analysis/pockets/notebook.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# purpose: to compare results from CASTp to fPocket
+# preconditions: get 2p54 results (plain pdb) from CASTp (email)
+#                for the same structure, calculate pockets locally with fpocket
+
+

0630b6223f5ca6671997afbac64cfc232c0da86a
assaff <assaf.faragy@mail.huji.ac.il>
2011-04-26 13:21:26 +0300

    fixed most bugs

--- analysis/pockets/notebook.sh
+++ analysis/pockets/notebook.sh
@@ -4,33 +4,74 @@
 # preconditions: get 2p54 results (plain pdb) from CASTp (email)
 #                for the same structure, calculate pockets locally with fpocket
 
-MAX_POCKET_DEPTH=2
+
+cleanup() {
+    rm -rf $1*
+}
+
+makePymolScript() {
+local pdbid=$1
+        echo -e \
+"load $pdbid.pdb; 
+"$(for f in $(ls $pdbid.pock*.pdb); do echo "load $f;"; done)" 
+select fpk, $pdbid.pock*.fpo*;
+select cstp, $pdbid.pock*.cast*;
+cmd.color(\"grey80\",\"$pdbid\");
+deselect;
+load ../../data/peptiDB/bound/boundSet/$pdbid.pdb, bound; 
+select peptide, bound and not chain A;
+cmd.color(\"magenta\",'peptide');
+cmd.extract(None,\"peptide\",zoom=0);
+cmd.show_as(\"cartoon\"   ,\"all\");
+cmd.disable('bound');
+cmd.spectrum(\"b\",selection=(\"fpk\"),quiet=0);
+cmd.spectrum(\"b\",selection=(\"cstp\"),quiet=0);
+cmd.show_as(\"spheres\"   ,\"fpk\");
+cmd.show_as(\"mesh\"      ,\"cstp\");" \
+ > $pdbid.fpk.cstp.pml
+}
+
+MAX_POCKET_DEPTH=4
 
 # cleanup
-selfname=$(basename $0)
-cp $selfname ..
-rm -rf *
-mv ../$selfname .
+#selfname=$(basename $0)
+#cp $selfname ..
+#mv ../$selfname .
 
-castp_pdbs=$(ls -d /vol/ek/assaff/workspace/peptalk/data/peptiDB/unbound/CastPAnalysis/CastPData/???? | cut -c 79-82)
+#castp_pdbs=$(ls -d /vol/ek/assaff/workspace/peptalk/data/peptiDB/unbound/CastPAnalysis/CastPData/???? | cut -c 79-82); cat $castp_pdbs
 
-#cat ../summaryTable_reduced.txt | grep 'BIGGEST' | cut -d' ' -f1 > biggestPocket.pdb_list.txt
-for pdbcode in $castp_pdbs; do #$(cat biggestPocket.pdb_list.txt); do
-
+#cat ../summaryTable_reduced.txt | grep 'IN_BIGGEST' | cut -d' ' -f1 > biggestPocket.pdb_list.txt; cat biggestPocket.pdb_list.txt
+for pdbcode in "$@"; do 
+    
+    cleanup $pdbcode
+    
     #unzip castpcalculation.zip
-    rm -rf $pdbcode*
-    cp /vol/ek/assaff/workspace/peptalk/data/peptiDB/unbound/CastPAnalysis/CastPData/$pdbcode/$pdbcode.* .
-    [ $? -ne 0 ] && continue
-
-    fpocket -f $pdbcode.pdb > /dev/null
-
+    castp_dir="/vol/ek/assaff/workspace/peptalk/data/peptiDB/unbound/CastPAnalysis/CastPData/$pdbcode/"
+    if [ ! -d $castp_dir ]; then
+        cleanup $pdbcode
+        echo "# $pdbcode insufficient CASTp data"
+        continue
+    fi
+    cp $castp_dir/* .
     cat $pdbcode.pocInfo | sort -nrk7 | nl -pv 0 | awk '{print $1,$4;}' > $pdbcode.vol_sa.pocRank
     cat $pdbcode.poc | grep '^ATOM' | awk '{print $6,$12}' > $pdbcode.resPocMap
+
+    fpocket -f $pdbcode.pdb > /dev/null 2>&1
+    pockets_dir=$pdbcode"_out"/pockets
+    if [[ $(ls $pockets_dir) == "" ]]; then
+        cleanup $pdbcode
+        echo "# $pdbcode insufficient Fpocket data"
+        continue
+    fi
+
     
     POCKET_DEPTH=$MAX_POCKET_DEPTH
-    FPOCKETS_NUM=$(ls $pdbcode"_out"/pockets/*.pdb | wc -l)
-    [ $FPOCKETS_NUM -lt $POCKET_DEPTH ] && POCKET_DEPTH=$FPOCKETS_NUM
-
+    FPOCKETS_DEPTH=$(( $(ls $pockets_dir/*pdb | wc -l) -1))
+    
+    if [[ $FPOCKETS_DEPTH -lt $POCKET_DEPTH ]]; then
+        POCKET_DEPTH=$FPOCKETS_DEPTH
+    fi
+    
     for i in $(seq 0 $POCKET_DEPTH); do 
 
         # fpocket per-pocket residue numbers
@@ -48,28 +89,7 @@ for pdbcode in $castp_pdbs; do #$(cat biggestPocket.pdb_list.txt); do
         cat $pdbcode.poc | grep '^ATOM' | grep -e "$pocId  POC" | sed -r "s/.....  ($pocId  POC)/ $i     \1/" > $pdbcode.pocket$i.castp.pdb
         cat $pdbcode"_out"/pockets/pocket$i"_atm.pdb" | grep '^ATOM' | sed -r "s/(.{60})/\1 $i/" > $pdbcode.pocket$i.fpocket.pdb
     done
-
-
-        echo -e \
-"load $pdbcode.pdb; 
-"$(for f in $(ls $pdbcode.pock*.pdb); do echo "load $f;"; done)" 
-select fpk, $pdbcode.pock*.fpo*;
-select cstp, $pdbcode.pock*.cast*;
-cmd.color(\"grey80\",\"$pdbcode\");
-deselect;
-load ../../data/peptiDB/bound/boundSet/$pdbcode.pdb, bound; 
-select peptide, bound and not chain A;
-cmd.color(\"magenta\",'peptide');
-cmd.extract(None,\"peptide\",zoom=0);
-cmd.show_as(\"cartoon\"   ,\"all\");
-cmd.disable('bound');
-cmd.spectrum(\"b\",selection=(\"fpk\"),quiet=0);
-cmd.spectrum(\"b\",selection=(\"cstp\"),quiet=0);
-cmd.show_as(\"spheres\"   ,\"fpk\");
-cmd.show_as(\"mesh\"      ,\"cstp\");" \
- > $pdbcode.fpk.cstp.pml
     
-
     # analyze recall and precision, crossing all top pockets
     for i in $(seq 0 $POCKET_DEPTH); do
         for j in $(seq 0 $POCKET_DEPTH); do
@@ -84,6 +104,7 @@ cmd.show_as(\"mesh\"      ,\"cstp\");" \
         done
     done
     sort -nrk3 fpk.cst.recall.$pdbcode.txt | head -1
-    #rm -rf $pdbcode*
-done > fpocket.vs.castp.recall.csv
+    makePymolScript $pdbcode
+    
+done #fpocket.vs.castp.recall.csv
 

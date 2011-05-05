58ec421309b37dd56b6089f1c2fddc16715e8ab9
assaff <assaf.faragy@mail.huji.ac.il>
2011-05-05 17:13:20 +0300

    show ligands as spheres

--- analysis/visual/visualize_data.sh
+++ analysis/visual/visualize_data.sh
@@ -37,7 +37,11 @@ function visualize_data_pair {
     local bound_chain_obj="bound_"$bound_pdb"_"$bc
     local unbound_chain_obj="unbound_"$unbound_pdb"_"$ubc
     local peptide_obj="peptide_"$bound_pdb"_"$pc
-
+    
+    #ligand objects
+    local bound_lig="ligands_"$bound_pdb
+    local unbound_lig="ligands_"$unbound_pdb
+    
     local pmlfile="$bound_pdb.$unbound_pdb.pml"
     #echo $pmlfile
     rm -f $pmlfile
@@ -52,7 +56,6 @@ function visualize_data_pair {
     #getPdbLocal $unbound_pdb > $unbound_pdb.pdb
     echo load $LOCAL_PDB_DIR/$midid_ub/pdb$unbound_pdb.ent.gz, $unbound_pdb_obj >> $pmlfile
     echo select $unbound_chain_obj, polymer and $unbound_pdb_obj and chain $ubc_obj >> $pmlfile
-
     echo align polymer and name ca and $bound_chain_obj, polymer and name ca and $unbound_chain_obj, quiet=0, object=\"aln_bound_unbound\", reset=1 >> $pmlfile
 
     echo select $peptide_obj, polymer and $bound_pdb_obj and chain $pc_obj >> $pmlfile
@@ -63,21 +66,23 @@ function visualize_data_pair {
     echo color blue, $unbound_chain_obj >> $pmlfile
     echo color red, $peptide_obj >> $pmlfile
 
-    echo show_as cartoon, all >> $pmlfile
+    echo "show_as cartoon, all" >> $pmlfile
 #    echo hide everything, not bound and not unbound and not peptide >> $pmlfile
     echo "orient" >> $pmlfile
+    echo "show sticks, sc. and $peptide_obj" >> $pmlfile
 #    echo "create $unbound_pdb.surface, $unbound_pdb_obj; color white, $unbound_pdb.surface; set transparency, 0.6, $unbound_pdb.surface; show_as surface, $unbound_pdb.surface; hide everything, $unbound_pdb.surface" >> $pmlfile
-    echo "symexp unbound_sym, $unbound_chain_obj, ($unbound_chain_obj), 5.0" >> $pmlfile
-    echo "symexp bound_sym, $bound_chain_obj, ($bound_chain_obj), 5.0" >> $pmlfile
-    echo "show_as cartoon, *_sym*; color white, sym*" >> $pmlfile
- #   echo "disable *_sym*" >> $pmlfile
+    echo "symexp unbound_sym, $unbound_pdb_obj, ($unbound_pdb_obj), 5.0" >> $pmlfile
+    echo "symexp bound_sym, $bound_pdb_obj, ($bound_pdb_obj), 5.0" >> $pmlfile
+    echo "show_as cartoon, *_sym*; color white, *_sym*; disable *_sym*" >> $pmlfile
 #    echo "center $peptide_obj" >> $pmlfile
 
-#    echo "run ColorByRMSD.py" >> $pmlfile
+    echo select $bound_lig, $bound_pdb_obj and not polymer and not resn HOH >> $pmlfile
+    echo select $unbound_lig, $unbound_pdb_obj and not polymer and not resn HOH >> $pmlfile
+    echo "show_as spheres, $bound_lig or $unbound_lig" >> $pmlfile
     #getRemarks $bound_pdb > remarks.$bound_pdb.info
     #getRemarks $unbound_pdb > remarks.$unbound_pdb.info
     
-#    pymol -q $pmlfile &
+    #pymol -q $pmlfile &
 }
 
 function visualize_table {

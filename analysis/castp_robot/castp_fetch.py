6e72a30e9b5b4da1bf7bf10ba7070ebbf207459f
assaff <assaf.faragy@mail.huji.ac.il>
2011-05-26 18:19:21 +0300

    bug fixing

--- analysis/castp_robot/castp_fetch.py
+++ analysis/castp_robot/castp_fetch.py
@@ -13,10 +13,10 @@ psswd = config.get('account', 'password')
 addrs = config.get('account', 'email')
 
 def fetch_by_keyword(kwd):
-    c = open_connection(hostn, uname, psswd, verbose=True)
+    c = imap_utils.open_connection(hostn, uname, psswd, verbose=True)
     messages = imap_utils.getMsgs(c, keyword=kwd)
     for msg in messages:
-        for attachement_filename, attachment_content in getAttachments(msg, filename_filter=None):
+        for attachement_filename, attachment_content in imap_utils.getAttachments(msg, filename_filter=None):
             print 'Saving %s...' % attachment_filename
             local_fd=open(attachment_filename, 'w')
             local_fd.write(attachment_content)

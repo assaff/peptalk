a80ac46c06d1ac191bebe8ab3a6795102da8814c
assaff <assaf.faragy@mail.huji.ac.il>
2011-06-13 16:03:47 +0300

    script to measure centroid distance between two PDB coordinate sets

---
+++ analysis/sandbox/centroid_dist.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+
+import sys, os
+import numpy as np
+
+def get_coords(filename):
+    fd = open(filename, 'r')
+    pdb_array = [line.split() for line in fd.readlines() if line.startswith('ATOM')]
+    fd.close()
+    coords = np.array([ map(float, atom[6:9]) for atom in pdb_array])
+    return coords
+
+def centroid(atom_coords):
+    centroid_coords = np.average(atom_coords, axis=0)
+    assert centroid_coords.ndim == 1
+    return centroid_coords
+
+def centroid_dist(filename1, filename2):
+    centroid1 = centroid(get_coords(filename1))
+    centroid2 = centroid(get_coords(filename2))
+    return np.linalg.norm(centroid1-centroid2)
+
+if __name__=='__main__':
+    for pocket_filename in sys.argv[2:]:
+        print pocket_filename[6:8], centroid_dist(sys.argv[1], pocket_filename)

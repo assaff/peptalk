89194af0cc4c488868a375c2552ce85a83f2a439
assaff <assaf.faragy@mail.huji.ac.il>
2011-06-23 17:52:37 +0300

    submit script functional, also fetches

--- analysis/castp_robot/castp_fetch.py
+++ analysis/castp_robot/castp_fetch.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 
 import sys, os, imaplib, email
+import tarfile
 import ConfigParser
 import imap_utils
 
@@ -12,16 +13,34 @@ uname = config.get('account', 'username')
 psswd = config.get('account', 'password')
 addrs = config.get('account', 'email')
 
+
+def poll(keyword, verbose=False):
+    c = imap_utils.open_connection(hostn, uname, psswd, verbose=verbose)
+    messages = imap_utils.getMsgs(c, keyword=keyword)
+    for msg in messages: return True
+    return False
+
+def fetch_by_job_id(jid):
+    filename_list = fetch_by_keyword(jid)
+    assert len(filename_list)==1
+    filename = filename_list[0]
+    assert filename == '%s.tar.gz' % jid
+    tf = tarfile.open(filename, mode='r:gz')
+    return tf
+
 def fetch_by_keyword(kwd):
     c = imap_utils.open_connection(hostn, uname, psswd, verbose=True)
     messages = imap_utils.getMsgs(c, keyword=kwd)
+    filenames_retrieved = []
     for msg in messages:
         for attachment_filename, attachment_content in imap_utils.getAttachments(msg, filename_filter=None):
             print 'Saving %s...' % attachment_filename
             local_fd=open(attachment_filename, 'w')
             local_fd.write(attachment_content)
             local_fd.close()
+            filenames_retrieved.append(attachment_filename)
     c.logout()
+    return filenames_retrieved
     
 if __name__ == '__main__':
     
--- analysis/castp_robot/castp_submit.py
+++ analysis/castp_robot/castp_submit.py
@@ -2,12 +2,18 @@
 
 import os, sys, shutil, time
 import re
-import urllib, urllib2
+import tarfile
+import urllib, urllib2, cookielib
+import MultipartPostHandler
+
 import castp_fetch
 
 CASTP_BASE_URL='http://sts.bioengr.uic.edu/castp/'
 CASTP_QUERY_FORM='query_process.php'
 CASTP_CALC_REQUEST_FORM='advcal.php'
+
+CASTP_JID_PREFIX = 'JID'
+CASTP_JID_LENGTH = 11
 
 def submit_pdbid(pdbid, email_address):
     url=CASTP_BASE_URL+CASTP_QUERY_FORM
@@ -17,9 +23,10 @@ def submit_pdbid(pdbid, email_address):
     success_pattern = re.compile('All result files have been sent to %s' % email_address)
     if not re.search(success_pattern, response_html):
         response.msg = 'FAIL'
+        return None
     return response
     
-    
+
 def submit_pdbfile(file_path, email_address):
     url=CASTP_BASE_URL + CASTP_CALC_REQUEST_FORM
     if not os.access(file_path, os.R_OK):
@@ -30,89 +37,66 @@ def submit_pdbfile(file_path, email_address):
         shutil.copy(file_path, os.getcwd())
         copied=True
     fd = open(tempfile)
-    params = {'submit_file':'Submit', 'email': email_address, 'visual':'emailonly', 'userfile' : fd}
-    print params
+    # using visual=jmol to have the job ID in the response URL
+    params = {'submit_file':'Submit', 'email': email_address, 'visual':'jmol', 'userfile' : fd}
     response = submit_form(url, params)
     if copied: os.remove(tempfile)
     fd.close()
+    jid_index = response.url.index(CASTP_JID_PREFIX)
+    jobid = response.url[jid_index:jid_index+CASTP_JID_LENGTH]
+    '''
     return response
+    success_pattern = re.compile('Results have been sent to')
+    if not re.search(success_pattern, response_html):
+        response.msg = 'FAIL'
+        return None
+    '''
+    return response, jobid
 
 def submit_form(form_url, params_dict):
+    cookies = cookielib.CookieJar()
+    opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cookies),
+                                    MultipartPostHandler.MultipartPostHandler)
+    response= opener.open(form_url, params_dict)
     # submit the calculation request to castp
-    params=urllib.urlencode(params_dict)
-    req=urllib2.Request(form_url, params)
-    response=urllib2.urlopen(req)
     return response
     
-def submit_pdb_and_fetch(pdbid):
+def submit_pdbid_and_fetch(pdbid):
     email_address = castp_fetch.addrs
-    
-    query_pdbid(pdbid, email_address)
+    submit_pdbid(pdbid, email_address)
     time.sleep(10)
     castp_fetch.fetch_by_keyword(pdbid)
-
-import httplib, mimetypes
-
-def post_multipart(host, selector, fields, files):
-    """
-    Post fields and files to an http host as multipart/form-data.
-    fields is a sequence of (name, value) elements for regular form fields.
-    files is a sequence of (name, filename, value) elements for data to be uploaded as files
-    Return the server's response page.
-    """
-    content_type, body = encode_multipart_formdata(fields, files)
-    h = httplib.HTTP(host)
-    h.putrequest('POST', selector)
-    h.putheader('content-type', content_type)
-    h.putheader('content-length', str(len(body)))
-    h.endheaders()
-    h.send(body)
-    errcode, errmsg, headers = h.getreply()
-    return h.file.read()
-
-def encode_multipart_formdata(fields, files):
-    """
-    fields is a sequence of (name, value) elements for regular form fields.
-    files is a sequence of (name, filename, value) elements for data to be uploaded as files
-    Return (content_type, body) ready for httplib.HTTP instance
-    """
-    BOUNDARY = '----------ThIs_Is_tHe_bouNdaRY_$'
-    CRLF = '\r\n'
-    L = []
-    for (key, value) in fields:
-        L.append('--' + BOUNDARY)
-        L.append('Content-Disposition: form-data; name="%s"' % key)
-        L.append('')
-        L.append(value)
-    for (key, filename, value) in files:
-        L.append('--' + BOUNDARY)
-        L.append('Content-Disposition: form-data; name="%s"; filename="%s"' % (key, filename))
-        L.append('Content-Type: %s' % get_content_type(filename))
-        L.append('')
-        L.append(value)
-    L.append('--' + BOUNDARY + '--')
-    L.append('')
-    body = CRLF.join(L)
-    content_type = 'multipart/form-data; boundary=%s' % BOUNDARY
-    return content_type, body
-
-def get_content_type(filename):
-    return mimetypes.guess_type(filename)[0] or 'application/octet-stream'
-
+    
+def submit_pdbfile_and_fetch(pdbfile):
+    email_address = castp_fetch.addrs
+    response, jid = submit_pdbfile(pdbfile, email_address)
+    received = False
+    curr_wait=0
+    received = castp_fetch.poll(jid)
+    while not received and curr_wait<4:
+        time.sleep(4)
+        curr_wait += 1
+        received = castp_fetch.poll(jid)
+    if received:
+        filenames = castp_fetch.fetch_by_keyword(jid)
+        
+    else:
+        raise ValueError('Could not find results for %s on server' % jid)
 
 if __name__ == '__main__':
     
     pdbid_pattern = re.compile('^[a-z0-9]{4}[a-z]?$', re.IGNORECASE)
     email_address = 'assaf.faragy@mail.huji.ac.il'
     for arg in sys.argv[1:]:
+        response = None
         if arg.endswith('.pdb') and os.access(arg, os.R_OK):
-            submit_pdbfile(arg, email_address)
-            print 'Submitted PDB file %s' % arg
+            response = submit_pdbfile(arg, email_address)
+            if response: print 'Submitted PDB file %s' % arg
         else:
             if len(arg)>=4 and pdbid_pattern.match(arg):
                 pdbid = arg.lower()
-                submit_pdbid(arg, email_address)
-                print 'Submitted PDB code %s' % arg
+                response = submit_pdbid(arg, email_address)
+                if response: print 'Submitted PDB code %s' % arg
             else:
                 print '%s is not a valid argument. Provide either a PDB code (with optional single chain) or a pdb file.' % arg
     print 'Done. Results will be sent to email %s' % email_address

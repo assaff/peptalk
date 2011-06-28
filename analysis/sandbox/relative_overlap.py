1150696878045f4f652bfb74e103d4ddb3e33325
assaff <assaf.faragy@mail.huji.ac.il>
2011-06-28 17:51:54 +0300

    tweaking the overlap function

--- analysis/sandbox/relative_overlap.py
+++ analysis/sandbox/relative_overlap.py
@@ -2,26 +2,28 @@
 
 import sys, os
 import numpy as np
-import scipy as sp
+from scipy.spatial.distance import cdist
 
+def coords_from_pdb_line(line):
+    x = float(line[30:38])
+    y = float(line[38:46])    
+    z = float(line[46:54])
+    return (x, y, z)
+    
 def get_coords(filename):
+    atom_filter = lambda line: line.startswith('ATOM')
     fd = open(filename)
-    pdb_array = [line.split() for line in fd.readlines() if line.startswith('ATOM')]
+    coords = np.array(map(coords_from_pdb_line, filter(atom_filter, fd.readlines())))
     fd.close()
-    coords = np.array([ map(float, atom[6:9]) for atom in pdb_array])
     return coords
 
-def pocket_overlap(pocket_filename, site_filename):
+def pocket_overlap(peptide_filename, pocket_filename):
     pocket_coords = get_coords(pocket_filename)
-    site_coords = get_coords(site_filename)
-    cdist_matrix = sp.spatial.distance.cdist(pocket_coords, site_coords)
-    sum_ri = 
-    return pocket_coords.shape[0] * np.power( ,-1)
-    
-def centroid(atom_coords):
-    centroid_coords = np.average(atom_coords, axis=0)
-    assert centroid_coords.ndim == 1
-    return centroid_coords
+    peptide_coords = get_coords(peptide_filename)
+    cdist_matrix = cdist(pocket_coords, peptide_coords)
+    mean_ri = cdist_matrix.min(axis=1).mean()
+    assert (mean_ri > 0)
+    return (1+0.1*pocket_coords.shape[0]) * np.power(mean_ri ,-1)
 
 def centroid_dist(filename1, filename2):
     centroid1 = centroid(get_coords(filename1))
@@ -32,4 +34,6 @@ def centroid_dist(filename1, filename2):
 
 if __name__=='__main__':
     for pocket_filename in sys.argv[2:]:
-        print '%s %.3f %s' % (pocket_filename[6:8], centroid_dist(sys.argv[1], pocket_filename), pocket_filename)
+        if not os.access(pocket_filename, os.R_OK):
+            continue
+        print '%s %.3f %s' % (pocket_filename[6:8].strip('_'), pocket_overlap(sys.argv[1], pocket_filename), pocket_filename)
